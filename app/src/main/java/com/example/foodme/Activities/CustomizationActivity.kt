0ffa91888fc282a09e972f4af66facf9232891ab
Lunatic <42777805+attractson@users.noreply.github.com>
2020-04-09 18:21:56 +0900

    danny_initial
    xml - done
    action plan
    - check box (save to firebase)
    - alarm : notification

--- app/src/main/java/com/example/foodme/Activities/CustomizationActivity.kt
+++ app/src/main/java/com/example/foodme/Activities/CustomizationActivity.kt
@@ -1,13 +1,62 @@
 package com.example.foodme.Activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.TimePickerDialog
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.foodme.R
+import kotlinx.android.synthetic.main.activity_customization.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class CustomizationActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customization)
+        // lunch
+        val pickLunchTimeBtn = findViewById<Button>(R.id.lunch_time)
+        val lunchTextView     = findViewById<TextView>(R.id.lunch_txt)
+        pickLunchTimeBtn.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                lunchTextView.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
+        // dinner
+        val pickDinTimeBtn = findViewById<Button>(R.id.din_time)
+        val dinTextView     = findViewById<TextView>(R.id.din_txt)
+        pickDinTimeBtn.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                dinTextView.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
+
+        cus_back.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // need to send data
+        cus_save.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
+
+
+
 }
